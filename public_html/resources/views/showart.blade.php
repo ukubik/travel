b7aa873e5cffea3d6286a637f69df230a5ff261d
Юрий и Наталья Кубышкины <ukubik.mail.ru>
2018-08-26 10:48:39 +0300

    added container in show article

--- public_html/resources/views/showart.blade.php
+++ public_html/resources/views/showart.blade.php
@@ -24,9 +24,12 @@
   <div class="row">
 
     <div class="col-md-9">
-      <div class="row mb-5">
-        <div class="col p-4">
-          {!! $article->content !!}
+
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col pt-4">
+            {!! $article->content !!}
+          </div>
         </div>
       </div>
 
