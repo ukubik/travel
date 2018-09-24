56bb9ef408ee20f5b1cf3aaadcf26f39f8a1a112
ukubik <ukubik@mail.ru>
2018-09-24 16:31:42 +0300

    change sign out icon in navbar

--- public_html/resources/views/layouts/navbar.blade.php
+++ public_html/resources/views/layouts/navbar.blade.php
@@ -52,7 +52,7 @@
               </span>
               <li class="nav-item" data-toggle="tooltip" data-placement="bottom" title="Выйти">
                   <a class="nav-link" href="{{ route('logout') }}"
-                     onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fa fa-caret-square-o-right" aria-hidden="true"></i></a>
+                     onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fa fa-sign-out" aria-hidden="true"></i></a>
                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                      @csrf
                  </form>
